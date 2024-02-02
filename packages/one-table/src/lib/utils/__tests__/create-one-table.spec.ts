464e9a83d776acb095f19e34443c0f8caa3c33c5
andrewgrewell <andrewgrewell@gmail.com>
2024-02-01 20:38:18 -0800

    chore: mock crypto in one-table tests

--- packages/one-table/src/lib/utils/__tests__/create-one-table.spec.ts
+++ packages/one-table/src/lib/utils/__tests__/create-one-table.spec.ts
@@ -1,6 +1,15 @@
 import { testTableSchema } from '../../../__fixtures__/schema.fixture';
 import { createOneTable } from '../create-one-table';
 
+jest.mock('../crypto', () => ({
+  oneTableCrypto: {
+    primary: {
+      cipher: 'cipher',
+      password: 'password',
+    },
+  },
+}));
+
 describe('createOneTable', () => {
   it('should return a OneTable instance', () => {
     const result = createOneTable({
