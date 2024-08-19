a4edd1a2c2abfa457a1428570b69152921af2a95
andrewgrewell <andrewgrewell@gmail.com>
2024-08-19 11:25:08 -0700

    chore: remove old portfolio site

--- apps/portfolio-web/jest.config.ts
+++ apps/portfolio-web/jest.config.ts
@@ -1,13 +1,11 @@
 /* eslint-disable */
 export default {
-  prettierPath: require.resolve('prettier-2'),
-  displayName: 'portfolio-browser',
-  preset: '../../../../jest.preset.js',
+  displayName: 'portfolio-web',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  collectCoverage: false,
-  coverageDirectory: '../../../../coverage/apps/portfolio/clients/browser',
+  coverageDirectory: '../../coverage/apps/portfolio-web',
 };
