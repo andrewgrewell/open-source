6d8cce014ae752789c9ab65ddf3b6d41a0a71cf6
andrewgrewell <andrewgrewell@gmail.com>
2024-08-21 08:56:53 -0700

    chore: lint

--- apps/portfolio-web/components/about.tsx
+++ apps/portfolio-web/components/about.tsx
@@ -10,12 +10,12 @@ export const About: FC = () => {
       <SectionSubheading className="max-w-2xl text-lg">
         I am a seasoned Software Engineer based in the Pacific Northwest with over a
         decade of professional experience. My journey in software development began in my
-        youth, and over the years, I've grown not just in technical expertise but also in
-        leadership. I have led and grown teams, establishing efficient processes that
-        ensure high-quality, maintainable software. I prioritize creating solutions that
-        are easy to extend, cost-effective to run, and, most importantly, make customers
-        happy. I am passionate about continually refining my craft, delivering software
-        that delights users and drives business success.
+        youth, and over the years, I&apos;ve grown not just in technical expertise but
+        also in leadership. I have led and grown teams, establishing efficient processes
+        that ensure high-quality, maintainable software. I prioritize creating solutions
+        that are easy to extend, cost-effective to run, and, most importantly, make
+        customers happy. I am passionate about continually refining my craft, delivering
+        software that delights users and drives business success.
       </SectionSubheading>
     </Section>
   );
