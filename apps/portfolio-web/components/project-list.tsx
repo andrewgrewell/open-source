b3df9771b0e4aa1360d0635bfce33bd4da1eb1fa
andrewgrewell <andrewgrewell@gmail.com>
2024-08-29 14:46:52 -0700

    feat: add sd project to list

--- apps/portfolio-web/components/project-list.tsx
+++ apps/portfolio-web/components/project-list.tsx
@@ -9,29 +9,41 @@ export interface Project {
   description: string;
   tags: string[];
   githubPath?: string;
+  href?: string;
+}
+
+interface GithubProject extends Project {
+  githubPath: string;
+  href: never;
+}
+
+interface HostedProject extends Project {
+  githubPath: never;
+  href: string;
 }
 
 export interface ProjectListProps {
-  projects: Project[];
+  projects: (GithubProject | HostedProject)[];
 }
 export const ProjectList: FC<ProjectListProps> = ({ projects }) => {
   return (
     <div className="grid grid-cols-2 max-md:grid-cols-1 gap-4">
-      {projects.map((project) => (
-        <Link
-          key={project.name}
-          href={`${PERSONAL_GITHUB_LINK}/${project.githubPath}`}
-          target="_blank"
-        >
-          <Card className="min-w-48">
-            <h4 className="font-heading font-bold mb-2">{project.name}</h4>
-            <p className="flex flex-1 mb-2">{project.description}</p>
-            <div className="py-2">
-              <TagList tags={project.tags} />
-            </div>
-          </Card>
-        </Link>
-      ))}
+      {projects.map((project) => {
+        const href = project.githubPath
+          ? `${PERSONAL_GITHUB_LINK}/${project.githubPath}`
+          : project.href;
+        return (
+          <Link key={project.name} href={href} target="_blank">
+            <Card className="min-w-48">
+              <h4 className="font-heading font-bold mb-2">{project.name}</h4>
+              <p className="flex flex-1 mb-2">{project.description}</p>
+              <div className="py-2">
+                <TagList tags={project.tags} />
+              </div>
+            </Card>
+          </Link>
+        );
+      })}
     </div>
   );
 };
