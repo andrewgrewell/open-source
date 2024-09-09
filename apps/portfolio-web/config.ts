33673d6a1947605ca977ea0f9852772542c2b742
andrewgrewell <andrewgrewell@gmail.com>
2024-09-09 12:33:30 -0700

    chore: update cv link

--- apps/portfolio-web/config.ts
+++ apps/portfolio-web/config.ts
@@ -3,4 +3,4 @@ export const PERSONAL_GITHUB_LINK = 'https://github.com/andrewgrewell';
 export const PERSONAL_LINKED_IN_LINK = 'https://www.linkedin.com/in/andrew-grewell/';
 
 export const CV_LINK =
-  'https://drive.google.com/file/d/1o2f-Dzb7fPkDDFZGZ6PFz2uR8nEYVOoW/view?usp=sharing';
+  'https://drive.google.com/file/d/1h3Dc2HW9qYXc20V_gL0L6T_6_DV61j2Z/view?usp=sharing';
